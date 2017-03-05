50ceba1910d24a047c1062d60245629c77baad99
Iommi Underwood <iommiund@gmail.com>
2017-03-05 11:09:49 +0100

    .

--- app/src/main/java/com/iaum00557329/androidims/androidims/InstallActivity.java
+++ app/src/main/java/com/iaum00557329/androidims/androidims/InstallActivity.java
@@ -2,12 +2,67 @@ package com.iaum00557329.androidims.androidims;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
-public class InstallActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class InstallActivity extends AppCompatActivity{
+
+    //View Objects
+    private Button scanResource;
+    private TextView textViewResource;
+
+    //code scanner object
+    private IntentIntegrator codeScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_install);
+
+        //View Objects
+        scanResource = (Button) findViewById(R.id.scanResource);
+        textViewResource = (TextView) findViewById(R.id.textViewResource);
+
+        //initializing scan object
+        codeScan = new IntentIntegrator(this);
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if code has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if code contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting value to resource textview
+                    textViewResource.setText(obj.getString("resource"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void scanResource(View view) {
+        //initiating the qr code scan
+        codeScan.initiateScan();
     }
 }
